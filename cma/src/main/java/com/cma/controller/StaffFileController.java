8df3f74527012adf8af39efc975fb2d4eb3df7d8
ShaFan <max799198985@163.com>
2018-05-19 14:16:22 +0800

    V1.0.1

--- cma/src/main/java/com/cma/controller/StaffFileController.java
+++ cma/src/main/java/com/cma/controller/StaffFileController.java
@@ -1,7 +1,11 @@
 package com.cma.controller;
 
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.cma.pojo.StaffFile;
 
 /**
  * 3.1.12 人员管理
@@ -12,14 +16,38 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/StaffFile")
 public class StaffFileController {
+	
 	/**
 	 * 获取全部人员信息
 	 * method: GET
 	 * 
 	 * @param null
-	 * @return PersonnelFile
+	 * @return List<StaffFile>
 	 */
-	public void getAll() {
-		
+	public List<StaffFile> getAll() {
+		return null;
 	}
+	
+	/**
+	 * 获取全部人员信息(不含图片)
+	 * method: GET
+	 * 
+	 * @param null
+	 * @return List<StaffFile>
+	 */
+	public List<StaffFile> getAllwithoutpics() {
+		return null;
+	}
+
+	/**
+	 * 添加人员的个人信息
+	 * method: GET
+	 * 
+	 * @param null
+	 * @return String
+	 */
+	public String addStaff() {
+		return null;
+	}
+
 }
