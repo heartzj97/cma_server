8993599a5dde4a212e902bce60e6c1460ebe0164
乔韦龙 <867949220@qq.com>
2018-05-19 13:00:27 +0800

    update Generator and create Java Class StaffTraining

---
+++ cma/src/main/java/com/cma/mapper/StaffTrainingMapper.java
@@ -0,0 +1,7 @@
+package com.cma.mapper;
+
+import com.cma.pojo.StaffTraining;
+import com.cma.util.GeneralMapper;
+
+public interface StaffTrainingMapper extends GeneralMapper<StaffTraining> {
+}
---
+++ cma/src/main/java/com/cma/pojo/StaffTraining.java
@@ -0,0 +1,154 @@
+package com.cma.pojo;
+
+import java.util.Date;
+import javax.persistence.*;
+
+@Table(name = "staff_training")
+public class StaffTraining {
+    @Id
+    private Long id;
+
+    @Column(name = "create_time")
+    private Date createTime;
+
+    @Column(name = "modify_time")
+    private Date modifyTime;
+
+    private String name;
+
+    private Date time;
+
+    private String location;
+
+    private String speaker;
+
+    private String content;
+
+    private String remarks;
+
+    /**
+     * @return id
+     */
+    public Long getId() {
+        return id;
+    }
+
+    /**
+     * @param id
+     */
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    /**
+     * @return create_time
+     */
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    /**
+     * @param createTime
+     */
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    /**
+     * @return modify_time
+     */
+    public Date getModifyTime() {
+        return modifyTime;
+    }
+
+    /**
+     * @param modifyTime
+     */
+    public void setModifyTime(Date modifyTime) {
+        this.modifyTime = modifyTime;
+    }
+
+    /**
+     * @return name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return time
+     */
+    public Date getTime() {
+        return time;
+    }
+
+    /**
+     * @param time
+     */
+    public void setTime(Date time) {
+        this.time = time;
+    }
+
+    /**
+     * @return location
+     */
+    public String getLocation() {
+        return location;
+    }
+
+    /**
+     * @param location
+     */
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    /**
+     * @return speaker
+     */
+    public String getSpeaker() {
+        return speaker;
+    }
+
+    /**
+     * @param speaker
+     */
+    public void setSpeaker(String speaker) {
+        this.speaker = speaker;
+    }
+
+    /**
+     * @return content
+     */
+    public String getContent() {
+        return content;
+    }
+
+    /**
+     * @param content
+     */
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    /**
+     * @return remarks
+     */
+    public String getRemarks() {
+        return remarks;
+    }
+
+    /**
+     * @param remarks
+     */
+    public void setRemarks(String remarks) {
+        this.remarks = remarks;
+    }
+}
