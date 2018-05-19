249afaf45e5f93fab2358246f5e78dbf615144c2
乔韦龙 <867949220@qq.com>
2018-05-19 11:46:40 +0800

    ORM

--- cma/src/main/java/com/cma/util/Generator.java
+++ cma/src/main/java/com/cma/util/Generator.java
@@ -1,5 +1,16 @@
 package com.cma.util;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.mybatis.generator.api.MyBatisGenerator;
+import org.mybatis.generator.config.Configuration;
+import org.mybatis.generator.config.xml.ConfigurationParser;
+import org.mybatis.generator.internal.DefaultShellCallback;
+
+
+
 /**
  * 代码生成器
  * 
@@ -7,9 +18,29 @@ package com.cma.util;
  */
 public class Generator {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public void generator() throws Exception{
 
+		List<String> warnings = new ArrayList<String>();
+		boolean overwrite = true;
+		//指定 逆向工程配置文件
+		File configFile = new File("src/main/resources/generatorconfig.xml"); 
+		ConfigurationParser cp = new ConfigurationParser(warnings);
+		Configuration config = cp.parseConfiguration(configFile);
+		DefaultShellCallback callback = new DefaultShellCallback(overwrite);
+		MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config,
+				callback, warnings);
+		myBatisGenerator.generate(null);
+
+	} 
+	
+	public static void main(String[] args) throws Exception {
+		try {
+			Generator generatorSqlmap = new Generator();
+			generatorSqlmap.generator();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
 	}
 
 }
