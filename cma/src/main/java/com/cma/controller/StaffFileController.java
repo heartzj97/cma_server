d711ce61ac08c852e04f9a3926db6cc82ed98720
ShaFan <max799198985@163.com>
2018-05-20 16:30:07 +0800

    Revert "Revert "V1.0.7""
    This reverts commit 0b282cec18dca9c43aacd7016643dc242b812e0b.

--- cma/src/main/java/com/cma/controller/StaffFileController.java
+++ cma/src/main/java/com/cma/controller/StaffFileController.java
@@ -1,26 +1,11 @@
 package com.cma.controller;
 
-<<<<<<< HEAD
-=======
-import java.io.BufferedOutputStream;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
->>>>>>> parent of cb5d551... V1.0.7
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
-<<<<<<< HEAD
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-=======
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
->>>>>>> parent of cb5d551... V1.0.7
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cma.pojo.StaffFile;
@@ -82,53 +67,6 @@ public class StaffFileController {
 	}
 	
 	/**
-<<<<<<< HEAD
-=======
-	 * 获取人员图片
-	 * method: GET
-	 * 
-	 * @param null
-	 * @return String
-	 */
-	public String getStaffPicture() {
-		return null;
-	}
-	
-	
-	/**
-	 * 添加人员图片
-	 * method: GET
-	 * 
-	 * @param StaffFile
-	 * @return String
-	 */
-	@RequestMapping("/addPicture")
-	public String addStaffPicture(@RequestParam("picture") MultipartFile picture) {
-		if (!picture.isEmpty()) {      
-            try {     
-                BufferedOutputStream out = new BufferedOutputStream(      
-                        new FileOutputStream(new File(      
-                                picture.getOriginalFilename())));      
-                System.out.println(picture.getName());    
-                out.write(picture.getBytes());      
-                out.flush();      
-                out.close();  
-            } catch (FileNotFoundException e) {      
-                e.printStackTrace();      
-                return "Fail" + e.getMessage();      
-            } catch (IOException e) {      
-                e.printStackTrace();      
-                return "Fail" + e.getMessage();      
-            }     
-            return "Success";      
-      
-        } else {      
-            return "Fail";      
-        }      
-	}
-	
-	/**
->>>>>>> parent of cb5d551... V1.0.7
 	 * 修改指定人员
 	 * method: POST
 	 * 
@@ -136,15 +74,9 @@ public class StaffFileController {
 	 * @return String
 	 */
 	@RequestMapping(value="/modify", method=RequestMethod.POST)
-<<<<<<< HEAD
 	public String modify(String staffName, StaffFile staffFile) {
 		if (staffFile != null) {
 			staffFileService.modify(staffName, staffFile);
-=======
-	public String modify(@RequestBody StaffFileParam staffFileParam) {
-		if (staffFileParam != null) {
-			staffFileService.modify(staffFileParam);
->>>>>>> parent of cb5d551... V1.0.7
 			return "Success";
 		}
 		else {
@@ -159,13 +91,8 @@ public class StaffFileController {
 	 * @param name
 	 * @return String
 	 */
-<<<<<<< HEAD
 	@RequestMapping(value="/delete/{name}", method=RequestMethod.POST)
 	public String delete(@PathVariable String staffName) {
-=======
-	@RequestMapping(value="/delete", method=RequestMethod.POST)
-	public String delete(@RequestParam("name") String staffName) {
->>>>>>> parent of cb5d551... V1.0.7
 		 staffFileService.delete(staffName);
 		 return "Success";
 	}
