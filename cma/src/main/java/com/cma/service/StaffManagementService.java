40db5ee4ef61fa376255eb861379ac4d28ac4cf3
wo0shi0da0lao <fuyang.fuyang@163.com>
2018-05-26 21:41:03 +0800

    V1.1.3

--- cma/src/main/java/com/cma/service/StaffManagementService.java
+++ cma/src/main/java/com/cma/service/StaffManagementService.java
@@ -9,8 +9,8 @@ import org.springframework.stereotype.Service;
 import com.cma.mapper.StaffFileMapper;
 import com.cma.mapper.StaffMapper;
 import com.cma.pojo.Staff;
+import com.cma.pojo.StaffFile;
 import com.cma.pojo.StaffFileExample;
-import com.cma.pojo.StaffFileExample.Criteria;
 
 @Service
 public class StaffManagementService {
@@ -28,18 +28,19 @@ public class StaffManagementService {
 	public List<Staff> getAllNoFile() {
 		List<Staff> list1 = new ArrayList<Staff>();
 		List<Staff> list2 = new ArrayList<Staff>();
+		List<StaffFile> fileList = new ArrayList<StaffFile>();
 		list1 = staffMapper.selectAll();
+		fileList = staffFileMapper.selectAll();
 		for (int i = 0;i < list1.size();i++) {
 			Long userId = list1.get(i).getId();
-			StaffFileExample staffFileExample = new StaffFileExample();
-			StaffFileExample.Criteria criteria = staffFileExample.createCriteria();
-			criteria.andUserIdEqualTo(userId);
-			if (staffFileMapper.selectByExample(staffFileExample).size() == 0)
+			int j = 0;
+			for (;j < fileList.size();j++) {
+				if (fileList.get(j).getUserId() == userId)
+					break;
+			}
+			if (j == fileList.size())
 				list2.add(list1.get(i));
 		}
-		
-		
-		
 		return list2;
 	}
 }
