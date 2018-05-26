ef0bd00229afe81cae94943a86e4d0e4df7f65a6
wo0shi0da0lao <fuyang.fuyang@163.com>
2018-05-26 21:34:28 +0800

    V1.1.2

--- cma/src/main/java/com/cma/controller/StaffManagementController.java
+++ cma/src/main/java/com/cma/controller/StaffManagementController.java
@@ -1,5 +1,43 @@
 package com.cma.controller;
 
-public class StaffManagementController {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cma.pojo.Staff;
+import com.cma.service.StaffManagementService;
+
+@RestController
+@RequestMapping("/StaffManagement")
+public class StaffManagementController {
+	
+	@Autowired
+	StaffManagementService staffManagementService;
+	
+	/**
+	 * 获取全部人员简易信息
+	 * method:GET
+	 * 
+	 * @param null
+	 * @return List<Staff>
+	 */
+	@GetMapping("/getAll")
+	public List<Staff> getAll() {
+		return staffManagementService.getAllInformation();
+	}
+	
+	/**
+	 * 获取全部无档案人员列表
+	 * method:GET
+	 * 
+	 * @param null
+	 * @return List<Staff>
+	 */
+	@GetMapping("/getNoFile")
+	public List<Staff> getNoFile() {
+		return staffManagementService.getAllNoFile();
+	}
 }
--- cma/src/main/java/com/cma/service/StaffManagementService.java
+++ cma/src/main/java/com/cma/service/StaffManagementService.java
@@ -1,5 +1,45 @@
 package com.cma.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.cma.mapper.StaffFileMapper;
+import com.cma.mapper.StaffMapper;
+import com.cma.pojo.Staff;
+import com.cma.pojo.StaffFileExample;
+import com.cma.pojo.StaffFileExample.Criteria;
+
+@Service
 public class StaffManagementService {
 
+	@Autowired
+	private StaffMapper staffMapper;
+	
+	@Autowired
+	private StaffFileMapper staffFileMapper;
+	
+	public List<Staff> getAllInformation() {
+		return staffMapper.selectAll();
+	}
+	
+	public List<Staff> getAllNoFile() {
+		List<Staff> list1 = new ArrayList<Staff>();
+		List<Staff> list2 = new ArrayList<Staff>();
+		list1 = staffMapper.selectAll();
+		for (int i = 0;i < list1.size();i++) {
+			Long userId = list1.get(i).getId();
+			StaffFileExample staffFileExample = new StaffFileExample();
+			StaffFileExample.Criteria criteria = staffFileExample.createCriteria();
+			criteria.andUserIdEqualTo(userId);
+			if (staffFileMapper.selectByExample(staffFileExample).size() == 0)
+				list2.add(list1.get(i));
+		}
+		
+		
+		
+		return list2;
+	}
 }
