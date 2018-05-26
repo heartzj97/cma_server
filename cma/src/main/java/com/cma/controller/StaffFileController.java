e38d329ebc30525e1572f23fcc94b531e49b8763
wo0shi0da0lao <fuyang.fuyang@163.com>
2018-05-26 23:09:23 +0800

    V1.1.5

--- cma/src/main/java/com/cma/controller/StaffFileController.java
+++ cma/src/main/java/com/cma/controller/StaffFileController.java
@@ -1,16 +1,53 @@
 package com.cma.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.cma.pojo.Result;
+import com.cma.pojo.StaffFile;
 import com.cma.service.StaffFileService;
 
 @RestController
 @RequestMapping("/StaffFile")
 public class StaffFileController {
-	//@Autowired
-	//StaffFileService staffFileService;
+	@Autowired
+	StaffFileService staffFileService;
 	
+	/**
+	 * 获取单个人员档案信息
+	 * method:GET
+	 * 
+	 * @param Long
+	 * @return Result
+	 */
 	
+	/*
+	 * FIXME
+	 */
+	@GetMapping("/getOne")
+	public Result getOne(@RequestBody Long id) {
+		return Result.ok(staffFileService.getOne(id));
+	}
+	
+	/**
+	 * 获取单个人员档案信息
+	 * method:GET
+	 * 
+	 * @param StaffFile
+	 * @return Result
+	 */
+	/*
+	 * FIXME
+	 */
+	@PostMapping("/addOne")
+	public Result addOne(@RequestBody StaffFile staffFile) {
+		if (staffFile != null) {
+			staffFileService.addOne(staffFile);
+			}
+		return Result.ok();
+	}
 }
---
+++ cma/src/main/java/com/cma/pojo/StaffFileGetOneParam.java
@@ -0,0 +1,30 @@
+package com.cma.pojo;
+
+public class StaffFileGetOneParam {
+	private Long id;
+	private String name;
+	private String fileId;
+	private String fileLocation;
+	private String fileImage;
+	
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public void setFileId(String fileId) {
+		this.fileId = fileId;
+	}
+	
+	public void setFileLocation(String fileLocation) {
+		this.fileLocation = fileLocation;
+	}
+	
+	public void setFileImage(String fileImage) {
+		this.fileImage = fileImage;
+	}
+	
+}
