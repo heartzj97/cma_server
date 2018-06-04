3699293061ad37d25736c81006c8431ef8d9c28a
ShaFan <max799198985@163.com>
2018-06-04 15:53:21 +0800

    V1.1.22

--- cma/src/main/java/com/cma/controller/StaffAuthorizationController.java
+++ cma/src/main/java/com/cma/controller/StaffAuthorizationController.java
@@ -1,6 +1,10 @@
 package com.cma.controller;
 
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,6 +18,48 @@ import com.cma.service.StaffAuthorizationService;
 public class StaffAuthorizationController {
 	@Autowired
 	StaffAuthorizationService staffAuthorizationService; 
+	
+	/**
+	 * 6.1
+	 * 获取全部授权记录信息
+	 * 
+	 * @param null
+	 * @return Result
+	 * @author qjx
+	 */
+	@GetMapping("/getAll")
+	public Result getAll() {
+		List<Map<String, Object>> data = staffAuthorizationService.getAll();
+		return Result.ok(data);
+	}
+	
+	/**
+	 * 6.2
+	 * 获取某员工全部授权记录信息
+	 * 
+	 * @param null
+	 * @return Result
+	 * @author qjx
+	 */
+	@GetMapping("/getAllByStaff")
+	public Result getAllByStaff(@RequestParam("id") Long id) {
+		List<Map<String, Object>> data = staffAuthorizationService.getAllByStaff(id);
+		return Result.ok(data);
+	}
+	
+	/**
+	 * 6.3
+	 * 获取全部授权记录信息
+	 * 
+	 * @param null
+	 * @return Result
+	 * @author qjx
+	 */
+	@GetMapping("/getOne")
+	public Result getOne(@RequestParam("authorizationId") Long authorizationId) {
+		Map<String, Object> data = staffAuthorizationService.getOne(authorizationId);
+		return Result.ok(data);
+	}
 	
 	/**
 	 * 6.5
--- cma/src/main/java/com/cma/service/StaffAuthorizationService.java
+++ cma/src/main/java/com/cma/service/StaffAuthorizationService.java
@@ -1,9 +1,17 @@
 package com.cma.service;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.cma.mapper.StaffAuthorizationMapper;
+import com.cma.mapper.StaffMapper;
+import com.cma.pojo.Staff;
+import com.cma.pojo.StaffAuthorization;
 import com.cma.pojo.StaffAuthorizationExample;
 import com.cma.pojo.StaffAuthorizationExample.Criteria;
 
@@ -11,7 +19,77 @@ import com.cma.pojo.StaffAuthorizationExample.Criteria;
 public class StaffAuthorizationService {
 	
 	@Autowired
+	StaffMapper staffMapper;
+	
+	@Autowired
 	StaffAuthorizationMapper staffAuthorizationMapper;
+	
+	//6.1
+	public List<Map<String, Object>> getAll() {
+		List<Map<String, Object>> res = new ArrayList<Map<String, Object>>();
+		List<StaffAuthorization> authorizations = staffAuthorizationMapper.selectAll();
+		for (StaffAuthorization authorization : authorizations) {
+			Staff authorizer = staffMapper.selectByPrimaryKey(authorization.getAuthorizationId());
+			Staff user = staffMapper.selectByPrimaryKey(authorization.getUserId());
+			Map<String, Object> record = new HashMap<String, Object>();
+			record.put("id", authorization.getUserId());
+			record.put("name", user.getName());
+			record.put("department", user.getDepartment());
+			record.put("position", user.getPosition());
+			record.put("authorizationId", authorization.getAuthorizationId());
+			record.put("authorizerId", authorizer.getId());
+			record.put("authorizerName", authorizer.getName());
+			record.put("content", authorization.getContent());
+			record.put("authorizerDate", authorization.getAuthorizerDate());
+			res.add(record);
+		}
+		
+		return res;
+	}
+	
+	//6.2
+	public List<Map<String, Object>> getAllByStaff(Long id) {
+		List<Map<String, Object>> res = new ArrayList<Map<String, Object>>();
+		StaffAuthorizationExample staffAuthorizationExample = new StaffAuthorizationExample();
+		Criteria criteria = staffAuthorizationExample.createCriteria();
+		criteria.andUserIdEqualTo(id);
+		List<StaffAuthorization> authorizations = staffAuthorizationMapper.selectByExample(staffAuthorizationExample);					
+		for (StaffAuthorization authorization : authorizations) {
+			Staff authorizer = staffMapper.selectByPrimaryKey(authorization.getAuthorizationId());
+			Staff user = staffMapper.selectByPrimaryKey(authorization.getUserId());
+			Map<String, Object> record = new HashMap<String, Object>();
+			record.put("id", authorization.getUserId());
+			record.put("name", user.getName());
+			record.put("department", user.getDepartment());
+			record.put("position", user.getPosition());
+			record.put("authorizationId", authorization.getAuthorizationId());
+			record.put("authorizerId", authorizer.getId());
+			record.put("authorizerName", authorizer.getName());
+			record.put("content", authorization.getContent());
+			record.put("authorizerDate", authorization.getAuthorizerDate());
+			res.add(record);
+		}
+		
+		return res;
+	}
+	
+	//6.3
+	public Map<String, Object> getOne(Long authorizationId) {
+		Map<String, Object> res = new HashMap<String, Object>();
+		StaffAuthorization authorization = staffAuthorizationMapper.selectByPrimaryKey(authorizationId);					
+		Staff authorizer = staffMapper.selectByPrimaryKey(authorization.getAuthorizationId());
+		Staff user = staffMapper.selectByPrimaryKey(authorization.getUserId());
+		res.put("id", authorization.getUserId());
+		res.put("name", user.getName());
+		res.put("department", user.getDepartment());
+		res.put("position", user.getPosition());
+		res.put("authorizationId", authorization.getAuthorizationId());
+		res.put("authorizerId", authorizer.getId());
+		res.put("authorizerName", authorizer.getName());
+		res.put("content", authorization.getContent());
+		res.put("authorizerDate", authorization.getAuthorizerDate());
+		return res;
+	}
 	
 	public void deleteOne(Long value) {
 		StaffAuthorizationExample staffAuthorizationExample = new StaffAuthorizationExample();
