5e0d47339170f396b89753c6136cf66a211a39b1
wo0shi0da0lao <fuyang.fuyang@163.com>
2018-06-04 19:07:30 +0800

    V1.1.24

--- cma/src/main/java/com/cma/service/StaffQualificationService.java
+++ cma/src/main/java/com/cma/service/StaffQualificationService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 import com.cma.mapper.StaffQualificationMapper;
 import com.cma.pojo.StaffQualification;
 import com.cma.pojo.StaffQualificationExample;
+import com.cma.pojo.StaffTraining;
 import com.cma.pojo.StaffQualificationExample.Criteria;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
@@ -17,6 +18,17 @@ public class StaffQualificationService {
 	@Autowired
 	private StaffQualificationMapper staffQualificationMapper;
 
+	//5.2
+	public int addOne(Map<String,String> params) {
+		Long userId = Long.parseLong(params.get("id"));
+		params.remove("id");
+		ObjectMapper objectMapper = new ObjectMapper();
+		StaffQualification staffQualification = objectMapper.convertValue(params, StaffQualification.class);
+		staffQualification.setUserId(userId);
+		staffQualificationMapper.insertSelective(staffQualification);
+		return 1;
+	}
+	
 	public void deleteOne(Long value) {
 		StaffQualificationExample staffQualificationExample = new StaffQualificationExample();
 		Criteria criteria = staffQualificationExample.createCriteria();
