6778a40242619745bfe3c0103422c78b0569b19a
ShaFan <max799198985@163.com>
2018-07-16 16:34:15 +0800

    V1.4.15

--- cma/src/main/java/com/cma/service/CertificateService.java
+++ cma/src/main/java/com/cma/service/CertificateService.java
@@ -1,20 +1,33 @@
 package com.cma.service;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
 import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.cma.dao.CertificateMapper;
 import com.cma.pojo.Certificate;
+import com.cma.pojo.ExternalReviewDocument;
 
 @Service
 public class CertificateService {
 	
 	@Autowired
 	private CertificateMapper certificateMapper;
+	
+	private static final String FILE_PATH_LIN = "/usr/java/project/file/external_review/";
 
 	/**
 	 * 1.1
@@ -26,16 +39,43 @@ public class CertificateService {
 	
 	/**
 	 * 1.2
+	 * @throws UnsupportedEncodingException 
 	 */
-	public Certificate getOne(Long id) {
-		
-		return null;
+	public ResponseEntity<InputStreamResource> getOne(Long id) throws UnsupportedEncodingException {
+		Certificate find = certificateMapper.selectByPrimaryKey(id);
+		InputStream inputStream = null;
+		try {
+			inputStream = new FileInputStream(new File(find.getFilePath()));
+			InputStreamResource inputStreamResource = new InputStreamResource(inputStream);
+			HttpHeaders headers = new HttpHeaders();
+			headers.add("Content-Type", "application/octet-stream");
+			String fileName = new String(find.getFilePath().getBytes("gbk"),"iso8859-1");
+			headers.add("Content-Disposition", "attachment;filename=" + fileName);
+			ResponseEntity<InputStreamResource> response = new ResponseEntity<InputStreamResource>(inputStreamResource, headers, HttpStatus.OK);
+			return response;
+			
+		} catch (FileNotFoundException e) {
+			return ResponseEntity.notFound().build();
+		}
 	}
 	
 	/**
 	 * 1.3
+	 * @throws IOException 
+	 * @throws IllegalStateException 
 	 */
-	public Integer addOne(MultipartFile file) {
+	public Integer addOne(String fileId, String fileName, MultipartFile file) throws IllegalStateException, IOException {
+		String name = file.getOriginalFilename();
+		File dest = new File(FILE_PATH_LIN + name);
+		if(dest.exists()) {
+			return 500;
+		}
+		file.transferTo(dest);
+		Certificate certificate = new Certificate();
+		certificate.setFileId(fileId);
+		certificate.setFileName(fileName);
+		certificate.setFilePath(FILE_PATH_LIN + name);
+		certificateMapper.insert(certificate);
 		return 200;
 	}
 	
