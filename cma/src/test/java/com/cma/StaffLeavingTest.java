33ba4dc09c350b6b75e095b3a1dab8bae14ddcec
ShaFan <max799198985@163.com>
2018-07-22 08:39:12 +0800

    V2.3.0

--- cma/src/test/java/com/cma/StaffLeavingTest.java
+++ cma/src/test/java/com/cma/StaffLeavingTest.java
@@ -16,7 +16,6 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import com.cma.util.TestUtil;
@@ -24,7 +23,7 @@ import com.cma.util.TestUtil;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
-@FixMethodOrder(MethodSorters.JVM)
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class StaffLeavingTest {
 	@Autowired
 	private MockMvc mockMvc;
@@ -35,9 +34,11 @@ public class StaffLeavingTest {
 	
 	private List<String> bodys;
 	
+	private String ADD_PATH = "D:\\Git\\Project\\private\\cma_server\\testdate\\user_add.json";
+	
 	@Before
     public void setUp() throws Exception {
-		testData = TestUtil.loadData("D:\\Git\\Project\\private\\user.json");
+		testData = TestUtil.loadData(ADD_PATH);
 		datas = TestUtil.getListMapByJson(testData);
 		bodys = new ArrayList<String>();
 		for (Map<String, Object> data : datas) {
@@ -47,7 +48,7 @@ public class StaffLeavingTest {
     }
 	
 	@Test
-	public void testAddOne() throws Exception {	
+	public void taAddOne() throws Exception {	
 		for (String body : bodys) {
 		    mockMvc.perform(MockMvcRequestBuilders.post("/StaffManagement/addOne")
 		    		.contentType(MediaType.APPLICATION_FORM_URLENCODED)
@@ -61,60 +62,11 @@ public class StaffLeavingTest {
 	}
 	
 	@Test 
-	public void testGetAll() throws Exception {
-		mockMvc.perform(MockMvcRequestBuilders.get("/StaffManagement/getOne"))
+	public void tbGetAll() throws Exception {
+		mockMvc.perform(MockMvcRequestBuilders.get("/StaffManagement/getAll"))
 	    .andExpect(MockMvcResultMatchers.status().isOk())
 	    //.andDo(MockMvcResultHandlers.print())
 	    .andReturn();
 	}
 	
-	@Test
-	public void testGetOne() throws Exception {
-		for (int i = 1; i <= datas.size(); i++) {
-			mockMvc.perform(MockMvcRequestBuilders.get("/StaffManagement/getOne")
-					.contentType(MediaType.APPLICATION_FORM_URLENCODED)
-		    		.param("id", String.valueOf(i)))
-		    .andExpect(MockMvcResultMatchers.status().isOk())
-		    .andExpect(MockMvcResultMatchers.jsonPath("$.code").exists())
-		    .andDo(MockMvcResultHandlers.print())
-		    .andReturn();
-		}
-	}
-	
-	@Test
-	public void testDeleteOne() throws Exception {
-		for (int i = 1; i <= datas.size(); i++) {
-			String id = "id=" + i;
-			mockMvc.perform(MockMvcRequestBuilders.post("/StaffManagement/deleteOne")
-		    		.contentType(MediaType.APPLICATION_FORM_URLENCODED)
-		    		.content(id))
-		    .andExpect(MockMvcResultMatchers.status().isOk())
-		    .andExpect(MockMvcResultMatchers.jsonPath("$.code").value(200))
-		    //.andDo(MockMvcResultHandlers.print())
-		    .andReturn();
-		}
-	}
-	
-	@Test
-	public void testModifyOne() throws Exception {
-//		List<Map<String, Object>> mdatas = TestUtil.getListMapByJson(TestUtil.loadData("D:\\Git\\Project\\private\\user_modify.json"));
-//		List<String> mbodys = new ArrayList<String>();
-//		for (Map<String, Object> data : mdatas) {
-//			String body = TestUtil.getUrlParamsByMap(data);
-//			mbodys.add(body);
-//		}
-		int i = 1;
-		for (String body : bodys) {
-			body += ("&id=" + i);
-			mockMvc.perform(MockMvcRequestBuilders.post("/StaffManagement/modifyOne")
-		    		.contentType(MediaType.APPLICATION_FORM_URLENCODED)
-		    		.content(body))
-		    .andExpect(MockMvcResultMatchers.status().isOk())
-		    .andExpect(MockMvcResultMatchers.jsonPath("$.code").value(200))
-		    .andDo(MockMvcResultHandlers.print())
-		    .andReturn();
-			i++;
-		}
-		
-	}
 }
